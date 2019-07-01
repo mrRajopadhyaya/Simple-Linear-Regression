ed1a3b6b77b025726ff8903c4747626fd0d21684
abhinabraj <abhinab@fusemachines.com>
2019-07-01 21:53:46 +0545

    best-fit line fix

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ class LinearRegression {
     this.canvas.addEventListener("click", this.handleCanvasClick);
     this.slopeBtn.addEventListener("click", this.calculateSlope);
     this.clearBtn.addEventListener("click", this.clear);
-    console.log(this.toolBar.getBoundingClientRect(), "toolbarWidth");
+
     let toolBarWidth =
       this.canvas.width - this.toolBar.getBoundingClientRect().width;
     let windowWidth = window.innerWidth - this.canvas.width;
@@ -26,6 +26,13 @@ class LinearRegression {
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
   };
 
+  handleCanvasClick = event => {
+    const rect = this.canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    this.drawPoints(x, y);
+  };
+
   drawPoints(x, y) {
     this.ctx.beginPath();
     this.ctx.fillStyle = "white";
@@ -35,20 +42,10 @@ class LinearRegression {
     this.dataPoints.push([x, y]);
   }
 
-  drawLine(x1, y1, x2, y2) {
-    this.ctx.beginPath();
-    this.ctx.moveTo(x1, y1);
-    this.ctx.lineTo(x2, y2);
-    this.ctx.lineWidth = 2;
-    // set line color
-    this.ctx.strokeStyle = "red";
-    this.ctx.stroke();
-    this.ctx.closePath();
-  }
-
   calculateSlope = () => {
     let xMean = this.calculateXMean();
     let yMean = this.calculateYMean();
+
     let numerator = 0;
     let denominator = 0;
     let slope = 0;
@@ -62,18 +59,31 @@ class LinearRegression {
     slope = numerator / denominator;
 
     c = yMean - slope * xMean;
+
     let x1 = 0;
     let y1 = slope * x1 + c;
 
     let x2 = this.canvas.width;
     let y2 = slope * x2 + c;
+
     this.drawLine(x1, y1, x2, y2);
   };
+
+  drawLine(x1, y1, x2, y2) {
+    this.ctx.beginPath();
+    this.ctx.moveTo(x1, y1);
+    this.ctx.lineTo(x2, y2);
+    this.ctx.lineWidth = 2;
+    // set line color
+    this.ctx.strokeStyle = "pink";
+    this.ctx.stroke();
+    this.ctx.closePath();
+  }
 
   calculateXMean = () => {
     let sum = 0;
     this.dataPoints.forEach(points => {
-      sum = +points[0];
+      sum = sum + points[0];
     });
     return sum / this.dataPoints.length;
   };
@@ -81,21 +91,15 @@ class LinearRegression {
   calculateYMean = () => {
     let sum = 0;
     this.dataPoints.forEach(points => {
-      sum = +points[1];
+      sum = sum + points[1];
     });
     return sum / this.dataPoints.length;
   };
 
   clear = () => {
+    this.dataPoints = [];
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.drawCanvas();
-  };
-
-  handleCanvasClick = event => {
-    const rect = this.canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-    this.drawPoints(x, y);
   };
 }
 new LinearRegression();
